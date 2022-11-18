9db303e8bc8f76159163543fc50af24cc9dc4c1e
javieg25 <javieg25@ucm.es>
2022-11-18 10:30:00 +0100

    Update P4_spark.py

--- Assignment 2/ej4/P4_spark.py
+++ Assignment 2/ej4/P4_spark.py
@@ -1,10 +1,31 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, lit, avg
 import csv
 
 spark = SparkSession.builder.appName('StockSummary').getOrCreate()
 
 googleDF = spark.read.options(inferSchema='True', delimiter=',', header='True').csv('ratings.csv')
 
-type1 = googleDF.filter(col('rating') < 1)
-type1.show()
+#parte 1
+films = googleDF.groupBy('movieId').avg('rating')
+
+#parte 2
+type1 = films.filter(col('avg(rating)') <= 1).select('movieId')
+type1.withColumn('Range', lit(1)).show()
+type1.write.csv('Range_1.csv')
+
+type2 = films.filter((col('avg(rating)') <= 2) & (col('avg(rating)') > 1)).select('movieId')
+type2.withColumn('Range', lit(2)).show()
+type2.write.csv('Range_2.csv')
+
+type3 = films.filter((col('avg(rating)') <= 3) & (col('avg(rating)') > 2)).select('movieId')
+type3.withColumn('Range', lit(3)).show()
+type3.write.csv('Range_3.csv')
+
+type4 = films.filter((col('avg(rating)') <= 4) & (col('avg(rating)') > 3)).select('movieId')
+type4.withColumn('Range', lit(4)).show()
+type4.write.csv('Range_4.csv')
+
+type5 = films.filter((col('avg(rating)') <= 5) & (col('avg(rating)') > 4)).select('movieId')
+type5.withColumn('Range', lit(5)).show()
+type5.write.csv('Range_5.csv')
